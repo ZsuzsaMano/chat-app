2ff12498eeb9ef51af611153d0005263ec809d8e
Mano <manoaway@gmail.com>
2020-12-12 13:45:39 +0100

    basic JSX

---
+++ src/components/InputBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const InputBar = () => {
+  return(
+    <div>
+    <form action="">
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+    <button>SEND</button>
+    </form>
+    </div>
+  )
+}
+
+export default InputBar;

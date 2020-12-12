d07bec07e9351ff7c7635167c8c14cd9f9b8d9dd
Mano <manoaway@gmail.com>
2020-12-12 18:48:57 +0100

    import Inputbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './styles/App.min.css';
 import MainBox from './components/MainBox';
+import InputBar from './components/InputBar';
 
 function App() {
   return (
     <div className="App">
         <MainBox/>
+        <InputBar/>
     </div>
   );
 }

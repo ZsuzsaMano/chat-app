973b6fe5b68c4718288a62a442ae5695864259d3
Mano <manoaway@gmail.com>
2020-12-12 17:12:15 +0100

    basic jsx

---
+++ src/components/ChatBubble.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatBubble = () =>{
+  return(
+  <div className="chatbubble">
+  <p className="chatbubble__name">Name</p>
+  <p className="chatbubble_text">Lorem ipsum dolor sit amet.</p>
+  <p className="chatbubble__date">dateObject</p>
+  </div>
+  )
+}

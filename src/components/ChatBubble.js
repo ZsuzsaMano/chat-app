7799cdf4867de0f60d3d71493438e869d563cbc1
Mano <manoaway@gmail.com>
2020-12-12 22:42:26 +0100

    class mymessage

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ChatBubble = () =>{
   const d = new Date().toUTCString();
   return(
-  <div className="chatbubble">
+  <div className="chatbubble mymessage">
   <p className="chatbubble__name">Name</p>
   <p className="chatbubble_text">Lorem ipsum dolor sit amet.</p>
   <p className="chatbubble__date">{d}</p>

cb56aaf1239dc5061db97ead44ec6502bc4a51b1
Mano <manoaway@gmail.com>
2020-12-13 21:31:39 +0100

    context

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './styles/App.min.css';
 import MainBox from './components/MainBox';
 import InputBar from './components/InputBar';
+import ChatContextProvider from './context/ChatContext';
 
 function App() {
   return (
     <div className="App">
         <MainBox/>
+        <ChatContextProvider>
         <InputBar/>
+        </ChatContextProvider>
     </div>
   );
 }

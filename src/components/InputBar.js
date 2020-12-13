cd5a834e83b1665c363e1cc47f6d39bdf97d3cca
Mano <manoaway@gmail.com>
2020-12-13 20:30:02 +0100

    getYourname

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const InputBar = () => {
+  const [yourName, setYourname] = useState('')
+
+  const getYourname = e =>{
+  const yournameValue = document.getElementById('yourname').value;
+  const yournameForm = document.querySelector('.yourname');
+  e.preventDefault();
+  setYourname(yournameValue);
+  yournameForm.innerHTML = `<p>${yourName}<p>`;}
+
   return(
     <div>
     <form action="" className="inputbar">
-    <textarea name="message" id="message" placeholder="Message"></textarea>
-    <button>SEND</button>
+      <div className="yourname">
+        <input type="text"id="yourname" name="yourname" placeholder="Your name"/>
+        <button onClick={getYourname}>Save</button>
+      </div>
+      <div className="message">
+        <textarea name="message" id="message" placeholder="Message"></textarea>
+        <button>SEND</button>
+      </div>
     </form>
     </div>
   )

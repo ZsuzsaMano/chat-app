d70ec6d93db151f3bfe866cbd48e19dd9aa46a69
Zsuzsa Lukacs <manoaway@gmail.com>
2020-12-14 11:25:20 +0100

    replace message name and text with variables

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -1,17 +1,17 @@
-import React,{useContext} from 'react';
-import {ChatContext} from '../context/ChatContext'
+import React, { useContext } from 'react';
+import { ChatContext } from '../context/ChatContext';
+import token from '../config';
 
+const ChatBubble = ({ message }) => {
 
-const ChatBubble = () =>{
-  const{yourName, setYourname} = useContext(ChatContext);
   const d = new Date().toUTCString();
-  return(
-  <div className="chatbubble mymessage">
-  <p className="chatbubble__name">{yourName}</p>
-  <p className="chatbubble_text">Lorem ipsum dolor sit amet.</p>
+  return (
+  <div className="chatbubble">
+  <p className="chatbubble__name">{message.author}</p>
+  <p className="chatbubble_text">{message.message}</p>
   <p className="chatbubble__date">{d}</p>
   </div>
-  )
-}
+  );
+};
 
 export default ChatBubble;
